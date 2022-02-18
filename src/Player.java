fee23d2427088c38d244835e81ca7be9ce348cfe
Saeed <saeed786129@hotmail.com>
2022-02-18 11:55:41 +0000

    added Player.java code

--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Player {
+import java.awt.*;
+
+public class Player {
+
+    GamePanel panel;
+
+    int x, y, width, height;
+    double xspeed, yspeed;
+    Rectangle hitBox;
+    boolean keyLeft, keyRight, keyUp, keyDown;
+
+    Player(int x, int y, GamePanel panel){
+        this.x = x;
+        this.y = y;
+        hitBox = new Rectangle(x,y,width,height);
+    }
+    
+    public void set(){
+        x += xspeed;
+        y += yspeed;
+        hitBox.x = x;
+        hitBox.y =y;
+    }
+
+    public void draw(Graphics2D gtd){
+
+        gtd.setColor(Color.BLACK);
+        gtd.fillRect(x, y, width, height);
+    }
 }
